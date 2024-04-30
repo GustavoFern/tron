2933c9d468301b62c9b8347272b57606c0e6d53c
GustavoFern <160290132+GustavoFern@users.noreply.github.com>
2024-04-30 01:45:17 +0000

    Version 3

---
+++ src/pantalla.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <thread>
+#include <ftxui/screen/screen.hpp>
+using namespace std;
+using namespace ftxui;
+
+int main(int argc, char const *argv[])
+{
+    Screen pantalla = Screen::Create(
+        Dimension::Full(),
+        Dimension::Full());
+    int x = 0;
+    while (true)
+    {
+        x++;
+        pantalla.Clear();
+        this_thread::sleep_for(0.1s);
+
+        pantalla.PixelAt(x,5).character = "      |\\---/|";
+        pantalla.PixelAt(x,6).character = "-->   | o_o |";
+        pantalla.PixelAt(x,7).character = "       \\_^_/";
+
+        pantalla.Print();
+        cout<<pantalla.ResetPosition();
+    }
+
+    return 0;
+}
